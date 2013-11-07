4879a22c24912f46485593b56ea1cc7ee499095b
matsasc <matsasc@student.matnat.uio.no>
2013-11-07 16:40:22 +0100

    Added some more tests

--- matsasc/js/skip-logic.js
+++ matsasc/js/skip-logic.js
@@ -1,5 +1,20 @@
-function skipLogic(){
+function question(){
     var visible;
     var requirements;
     var formelemList;
+    var question;
+    var answear;
+}
+
+function checkRequirements(question){
+    return question.requirements;
+}
+
+function addRequirements(question, requirement){
+    question.requirements.push(requirement);
+}
+
+function showQuestion(question){
+    document.write(question);
+    document.write(answear);
 }
