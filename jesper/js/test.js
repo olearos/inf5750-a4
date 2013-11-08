4af2183d69746ad62d2a551f4ec02e167099cde5
Jesper Vestlie <jesperov@ifi.uio.no>
2013-11-08 11:12:50 +0100

    Commit before pull

--- jesper/js/test.js
+++ jesper/js/test.js
@@ -4,14 +4,14 @@ function getData() {
     //window.open("http://apps.dhis2.org/demo/api/attributeTypes.json");
     document.write("<p>This is a paragraph</p>");
     
-    //authenticate();
-
+    authenticate();
+    
     $.ajax({
 	type: "GET",
-	//dataType: "json",
-	url: "http://apps.dhis2.org/demo/api/attributeTypes.json",
+	dataType: "jsonp",
+	url: "http://apps.dhis2.org/demo/api/attributeTypes.jsonp",
     })
-
+    
 	.done(function(json) {
 	    printData(json);
 	})
@@ -22,23 +22,49 @@ function getData() {
     document.write("<p>END</p>");
 }
 
-//function authenticate() {
-//    jQuery(document).ready(function() {
-//	$.post("http://apps.dhis2.org/demo/dhis-web-commons/security/login.action",{
-//	    j_username: "admin", j_password: "district"
-//	});
-  //  });
-//}
+function authenticate() {
+    //var xhRequest = new XMLHttpRequest();
+    //xhRequest.open('POST', 'http://apps.dhis2.org/demo/dhis-web-commons-security/login.action', true, "admin", "district");
+    //xhRequest.setRequestHeader("Content-type", "text/javascript");
+    //xhRequest.withCredentials = "true";
+    //xhRequest.send("j_username=admin&j_password=district");
+    //xhRequest.send();
+    //console.log(xhRequest);
 
-//function authenticate() {
-//    jQuery(document).ready(function() {
-//	$.post("http://apps.dhis2.org/demo//dhis-web-commons-security/login.action?authOnly=true",{
+    var username = "admin";
+    var password = "district";
+    var tok = username + ':' + password;
+    var hash = btoa(tok);
+    
+    $.ajax({
+	type: "POST",
+	url:"/api/currentUser",
+	data:"jsonp",	
+
+	headers: {
+	    Authorization: "Basic " + hash,
+	},
+	
+    })
+
+	.done(function(json) {
+	    printData(json);
+	})
+	.fail(function() {
+	    alert("Something went wrong 2!");
+	});
+    
+    //jQuery(document).ready(function() {
+//	$.post("http://apps.dhis2.org/demo/dhis-web-commons-security/login.action",{
 //	    j_username: "admin", j_password: "district"
-//	});
-//    });
-//}
+//	}
+  //     );
+    //});
+}
+
 
 function printData(json) {
+    //localStorage.setItem('json_test', JSON.stringify(json));
     console.log(json); 
 }
 
