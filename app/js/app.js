99a2896bcdc648214ee8c6bbdde53d3626fb15d0
Lars Øyvind Hagland <larsoc@gmail.com>
2013-11-26 01:55:02 +0100

    js/app.js - The main application is still broken. No matter what I do I can't seem to get it to send the Authorization header. The code is kind of a mess currently, I have tried to comment what works and not.

--- app/js/app.js
+++ app/js/app.js
@@ -1,21 +1,182 @@
-
-username = "admin";
-password = "district";
+'use strict';
 
 
-function LoginCtrl( $scope ) {
-   $scope.login = function() {
-      ajaxFetch( "currentUser" )
-         .success( function() {
-            ajaxFetch( "programStages.json" );
-            // populate ??
-         })
-         .fail( function() {
-            $scope.loginError = "Login failed, please try again";
-         });
-   }
-}
+// TODO Remove before upload
+var username = 'admin';
+var password = 'district';
+var dhisAPI = 'http://apps.dhis2.org/demo';
+// console.log('API: '+dhisAPI);
 
+var login = 'Basic ' + btoa( username + ":" + password);
+
+
+/// Services
+
+var skipLogicServices = angular.module( 'skipLogic.services', ['ngResource'] );
+
+// Does not work
+skipLogicServices.factory( 'dhisResourceApi', function($resource) {
+         var api = $resource(
+            /*dhisAPI +*/ '/x_api/:id.json',
+            {}, // {callback:'JSON_CALLBACK'},
+            // TODO Remove headers before upload
+            {
+               get: {
+                     method:'JSONP',
+                     headers: {
+                        authorization: login
+                     }
+               },
+               save: {
+                  method:'POST',
+                  headers: {
+                     authorization: login
+                  }
+               }
+            }
+            );
+         return api;
+      });
+
+skipLogicServices.factory( 'dhisHttpApi', ['$http', function($http) {
+      var api = {
+         fetch: function(id) {
+            return $http.jsonp({
+//               method: "JSONP",
+               url: dhisAPI + '/api/' + id /*'programStages'*/ + '.json',
+//               dataType: "json",
+               headers: {
+                  "Authorization": login
+               }
+            }).success(function(data, status, headers, config) {
+                  return data;
+            }).failure(function(data, status, headers, config) {
+                  return status; // alert("API failure");
+            });
+         }
+      };
+      return api;
+}]);
+
+
+
+/// Controllers
+
+var skipLogicControls = angular.module('skipLogic.controllers', []);
+
+// This works apart from sending auth header
+skipLogicControls.controller('selectFormCtrl', ['$scope', '$http', function($scope, $http) {
+
+      $http({
+         url: dhisAPI + '/api/programStages' + '.json',
+         method: "GET",
+         dataType: "json",
+         accept: "text/json",
+         withCredentials: "true",
+         headers: {
+            'Authorization': "Basic YWRtaW46ZGlzdHJpY3Q=" // login
+         }
+      }).success( function(data) {
+         console.log(data);
+         $scope.dhisResult = data.programStages;
+      });
+}]);
+
+/*
+// This is a mess, has never worked
+skipLogicControls.controller('selectFormCtrl', ['$scope', '$http', 'dhisHttpApi', function($scope, $http, dhisHttpApi) {
+
+      $http({
+         url: dhisAPI + '/api/programStages' + '.json',
+         method: "JSONP",
+         dataType: "text/json",
+//         headers: {
+//            Authorization: login
+//        }
+      }).success( function(data) {
+         $scope.dhisResult = data;
+      });
+//      response = dhisApi.fetch('programStages')
+//   .then( function(data) { // (function(response) {
+//         $scope.dhisResult = data; // data; //.results;
+//   }, function(data) {
+//      alert("API failure");
+//   });
+   //   }) //, function() {
+//         console.log("Error: Not logged in");
+      }]);
+//   }]);
+
+
+// This has worked (not this particular implementation, but the service is currently broken. Headers are disfunctional.
+skipLogicControls.controller('selectFormCtrl', ['$scope', '$http', 'dhisResourceApi', function($scope, $http, dhisResourceApi) {
+   console.log("Controller");
+   dhisResourceApi({id: 'programStages'}, function(data) {
+      console.log(data);
+      $scope.dhisResult = data.programStages;
+   });
+//   var Api = dhisResourceApi();
+//   Api.action({});
+}]);
+*/
+
+skipLogicControls.controller('fillFormCtrl', ['$scope', 'dhisApi', function($scope) {
+   // TODO
+   }]);
+
+skipLogicControls.controller('editLogicCtrl', ['$scope', 'dhisApi', function($scope) {
+   // TODO
+   }]);
+
+
+
+/// Application module
+
+var skipLogic =  angular.module('skipLogic', [
+//      'ngResource',
+      'ngRoute',
+      'skipLogic.controllers',
+      'skipLogic.services']);
+
+// Configure HTTP headers, this actually works, apart for Authorization :S
+skipLogic.config(function($httpProvider) {
+   $httpProvider.defaults.headers.common = {
+      'Accept': 'application/json, text/plain, * / *',
+      'Data': '',
+      'Authorization': "Basic YWRtaW46ZGlzdHJpY3Q="}; // login};
+   console.log($httpProvider.defaults);
+});
+
+skipLogic.config(function($routeProvider) {
+      $routeProvider
+
+        .when('/',
+            {controller: 'selectFormCtrl',
+               templateUrl: 'view/selectForm.html'})
+
+         .when('/fillForm/:formId',
+            {controller: 'fillFormCtrl',
+               templateUrl: 'view/fillForm.html'})
+
+         .when('/editLogic/:formId',
+            {controller: 'editLogicCtrl',
+               templateUrl: 'view/editLogic.html'})
+
+         .otherwise({redirectTo:'/'});
+   });
+
+
+
+
+// Everything below here are notes
+
+/*
+   $scope.dhisResult = [
+   {name:"A"},
+   {name:"B"},
+   {name:"C"}
+   ];*/
+//}
 
 
 /*
@@ -32,31 +193,9 @@ module.factory( 'myService', function( $http ) {
    }
 });
 
-
 module.controller( 'MyCtrl', function( $scope, myService ) {
    $scope.foos = myService.getFoos().then( function( foos ) {
       $scope.foos = foos;
    });
 });
-
-
-// Old school ajax(?)
-function ajaxFetch( target ) {
-   var xhReq = new CMLHttpRequest();
-   xhReq.open( "GET", "/api/" + target, false );
-   xhReq.setRequestHeader( "Authorization", "Basic " + btoa( username + ":" + password ) )
-   xhReq.send();
-   var serverResponse = JSON.parse(xhReq.responseText);
-}
-
-/*
-
-
-/**
- * For encoding utf8 to b64, btoa() cannot be used directly with utf8
- * (if using input fields and utf8 encoding in browser)
- */
-function utf8_to_b64( str ) {
-	return btoa(unescape(encodeURIComponent( str )));
-}
-
+*/
